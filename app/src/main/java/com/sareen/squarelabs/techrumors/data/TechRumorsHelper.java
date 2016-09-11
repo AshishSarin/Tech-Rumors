c5b0bce0262c0108bdbd6b5b86f8342af7aa9b2d
AshishSarin <asareen007@gmail.com>
2016-09-11 20:32:41 +0530

    Implemented TechRumorsHelper Class

--- app/src/main/java/com/sareen/squarelabs/techrumors/data/TechRumorsHelper.java
+++ app/src/main/java/com/sareen/squarelabs/techrumors/data/TechRumorsHelper.java
@@ -3,6 +3,7 @@ package com.sareen.squarelabs.techrumors.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.sareen.squarelabs.techrumors.data.TechRumorsContract.SavedArticlesEntry;
 
 /**
  * Created by Ashish on 11-09-2016.
@@ -22,12 +23,22 @@ public class TechRumorsHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase db)
+    {
+        final String SQL_CREATE_SAVED_ARTICLES_TABLE = "CREATE TABLE "
+                + SavedArticlesEntry.TABLE_NAME + " ("
+                + SavedArticlesEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT"
+                + SavedArticlesEntry.COLUMN_POST_TITLE + " TEXT NOT NULL"
+                + ");";
 
+        db.execSQL(SQL_CREATE_SAVED_ARTICLES_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        // TODO : Modify this so that user data is not lost when database schema is changed
+        db.execSQL("DROP TABLE IF EXISTS " + SavedArticlesEntry.TABLE_NAME);
+        onCreate(db);
     }
 }
